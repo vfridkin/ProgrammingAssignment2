a7f18781f98cceb779a4d03ced53501705565b22
Vlad Fridkin <vladislavfridkin@gmail.com>
2015-12-14 14:46:19 +1100

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Description:
+  ## store a matrix and its inverse in cache to avoid recalculation
 
-## Write a short comment describing this function
+## makeCacheMatrix: 
+  ## defines functions for setting/getting the matrix and its inverse in cache
+  ## returns list of these functions
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(m = matrix()) {
+  ## init mi: matrix inverse
+    mi <- NULL 
 
+  ## store matrix to cache
+      set <- function(y) {
+      m <<- y
+      mi <<- NULL
+    }
+
+  ## return matrix from cache
+    get <- function() m
+
+  ## store inverse to cache
+    setInverse <- function(inverse) mi <<- inverse
+
+  ## return inverse from cache    
+    getInverse <- function() mi
+
+  ## return list of functions  
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve:
+  ## retrieves the matrix inverse from cache, or if NULL solves and stores the inverse
+  ## returns inverse
+  
+cacheSolve <- function(m, ...) {
+  ## get matrix inverse from cache
+    mi <- m$getInverse()
+    if(!is.null(mi)) {
+      message("getting cached matrix inverse")
+      return(mi)
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## cache is NULL, so solve for matrix inverse
+    tempMatrix <- m$get()
+    mi <- solve(tempMatrix) ## assignment assumes matrix is always invertible
+    m$setInverse(mi)
+    mi
 }
+
+
